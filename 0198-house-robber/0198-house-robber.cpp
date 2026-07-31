85d885c3aa5b3d27f90b942961adaf017dd6893a
Mridul Gehlot <mridulgehlot03@gmail.com>
2026-07-31 13:03:11 +0530

    Time: 0 ms (100%), Space: 10.1 MB (90.06%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,13 +3,39 @@ public:
     unordered_map<int,int> mp;
     int solve(vector<int>& nums,int idx)
     {
-        if(idx>=nums.size()) return 0;
         if(mp.count(idx)) return mp[idx];
-        int pick=nums[idx]+solve(nums,idx+2);
-        int nopick=solve(nums,idx+1);
-        return mp[idx]=max(pick,nopick);
+        int a,b;
+        a=b=0;
+        for(int i=idx;i<nums.size();i++)
+        {
+            a=max(a,nums[i]+solve(nums,i+2));
+            b=max(b,solve(nums,i+1));
+        }
+        return mp[idx] = max(a,b);
     }
     int rob(vector<int>& nums) {
-        return solve(nums,0);
+        /*
+        vector<int> dp(nums.size(),0);
+        //return solve(nums,0);
+        int a,b;
+        a=b=0;
+        for(int i=0;i<nums.size();++i)
+        {
+            a=nums[i];
+            if(i>1) a+=dp[i-2];
+            if(i>0) b=dp[i-1];
+            dp[i]=max(a,b);
+        }
+        return dp[nums.size()-1];
+        */
+        int prev1,prev2;
+        prev1=prev2=0;
+        for(int x:nums)
+        {
+            int curr=max(prev1,prev2+x);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
     }
 };
