b763c5597becf7a2ac1df58c5b61baec4d7c48c4
Mridul Gehlot <mridulgehlot03@gmail.com>
2026-08-16 22:02:29 +0530

    Time: 3 ms (75.38%), Space: 131.2 MB (92.89%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        vector<int> freq(3,0);
+        for(int x:stones)
+        {
+            freq[x%3]++;
+        }
+        if(freq[0]%2==0) return freq[1]>0 && freq[2]>0;
+        return abs(freq[1]-freq[2])>2;
+    }
+};
